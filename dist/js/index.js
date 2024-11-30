b92eca35f8c3fb6089372369556775cfb5c70fbb
Yura <stauber.j@mail.ru>
2024-11-30 19:02:56 +0300

    added hamburger and mobile menu with scripts

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,25 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function (e) {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mob-menu');
+    const mobileMenuItems = document.querySelectorAll('.mob-list__item');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+
+        if (hamburger.classList.contains('hamburger_active')) {
+            mobileMenu.classList.add('mob-menu_active');
+        } else {
+            mobileMenu.classList.remove('mob-menu_active');
+        }
+
+        mobileMenuItems.forEach(item => {
+            item.addEventListener("click", (e) => {
+                e.preventDefault();
+                hamburger.classList.remove('hamburger_active');
+                mobileMenu.classList.remove('mob-menu_active');
+            })
+        })
+    });
+});
